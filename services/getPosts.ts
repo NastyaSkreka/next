f0fbc4050143b8487187cfee9079e4ab78f756d6
Nastya <skreka.nastya@gmail.com>
2023-10-04 00:25:26 +0300

    add search panel use client hooks

---
+++ services/getPosts.ts
@@ -0,0 +1,19 @@
+export const getAllPosts = async() => {
+    const response =  await fetch("https://jsonplaceholder.typicode.com/posts", {
+        next: {
+            revalidate: 60,
+        }
+    })
+
+    if (!response.ok) throw new Error("Unable to fetch posts!")
+
+    return response.json();
+}
+
+export const getPostBySearch = async(search: string) => {
+    const response =  await fetch( `https://jsonplaceholder.typicode.com/posts?q=${search}`)
+
+    if (!response.ok) throw new Error("Unable to fetch post!")
+
+    return response.json();
+}
