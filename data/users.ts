d697e954ccb72c1398f1e2514eb555dc71e5e38b
Nastya <skreka.nastya@gmail.com>
2023-10-10 17:06:56 +0300

    add form

---
+++ data/users.ts
@@ -0,0 +1,23 @@
+export const users = [
+    {
+      id: "1",
+      email: "skreka.nastya@gmail.com",
+      name: "Skreka Nastya",
+      password: "12345",
+      role: "admin",
+    },
+    {
+      id: "2",
+      email: "admin@pcgramota.ru",
+      name: "Super Admin",
+      password: "12345",
+      role: "admin",
+    },
+    {
+      id: "3",
+      email: "any@gmail.com",
+      name: "Just a Guest",
+      password: "12345",
+      role: "guest",
+    },
+  ];
